40b17c968a2e220d570ac054031a61cdd2e68fdb
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-14 17:38:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,18 @@
 class Person
 
+  attr_accessor :name
+
   def initialize(name)
     @name = name
   end
   
-  def name=(name)
-    @name = name
-  end
+  # def name=(name)
+  #   @name = name
+  # end
   
-  def name
-    @name
-  end
+  # def name
+  #   @name
+  # end
   
 
 end
